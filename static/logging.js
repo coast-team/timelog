99c875eae0c7bd458f2493c57c7c20e8d8aa7cc5
Victorien Elvinger <victorien.elvinger@inria.fr>
2016-03-16 17:07:27 +0100

    Fix json representation and add doc id

--- static/logging.js
+++ static/logging.js
@@ -19,26 +19,27 @@ var Logger = (function () {
     Integrated_operation_tag: "integrated",
 
     // Access
-    _representation_of: function (a_op_id, a_site_id, a_timestamp, a_event_tag) {
+    _representation_of: function (a_op_id, a_document_id, a_site_id, a_timestamp, a_event_tag) {
       return {
         id: a_op_id,
-        site_id: a_site_id,
+        doc: a_document_id,
+        site: a_site_id,
         timestamp: a_timestamp,
         event: a_event_tag
       };
     },
 
     // change
-    log_sent_operation: function (a_op_id, a_site_id, a_timestamp) {
-      this._buffer_[this.buffer.length] = this._representation_of (a_op_id, a_site_id, a_timestamp, this.Sent_operation_tag);
+    log_sent_operation: function (a_op_id, a_document_id, a_site_id, a_timestamp) {
+      this._buffer_[this.buffer.length] = this._representation_of (a_op_id, a_document_id, a_site_id, a_timestamp, this.Sent_operation_tag);
     },
 
-    log_received_operation: function (a_op_id, a_site_id, a_timestamp) {
-      this._buffer_[this.buffer.length] = this._representation_of (a_op_id, a_site_id, a_timestamp, this.Received_operation_tag);
+    log_received_operation: function (a_op_id, a_document_id, a_site_id, a_timestamp) {
+      this._buffer_[this.buffer.length] = this._representation_of (a_op_id, a_document_id, a_site_id, a_timestamp, this.Received_operation_tag);
     },
 
-    log_integrated_operation: function (a_op_id, a_site_id, a_timestamp) {
-      this._buffer_[this.buffer.length] = this._representation_of (a_op_id, a_site_id, a_timestamp, this.Integrated_operation_tag);
+    log_integrated_operation: function (a_op_id, a_document_id, a_site_id, a_timestamp) {
+      this._buffer_[this.buffer.length] = this._representation_of (a_op_id, a_document_id, a_site_id, a_timestamp, this.Integrated_operation_tag);
     },
 
     flush: function () {
